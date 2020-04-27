7a5bca61ed45482aefe2aecb0a7b1d4f35ba5856
tuyenhoang0320i1 <63198356+tuyenhoang0320i1@users.noreply.github.com>
2020-04-28 00:00:25 +0700

    format code

--- CS-DrMario.js
+++ CS-DrMario.js
@@ -85,18 +85,18 @@ Piece.prototype.unDraw = function () {
 // move Down the peice
 
 Piece.prototype.moveDown = function () {
-    if (!this.collision(0,1,this.activeTetromino)) {
+    if (!this.collision(0, 1, this.activeTetromino)) {
         this.unDraw();
         this.y++;
         this.draw();
-    }else {
+    } else {
         // we lock the piece and generate a new one
     }
 }
 
 // move Right the piece
 Piece.prototype.moveRight = function () {
-    if (!this.collision(1,0,this.activeTetromino)) {
+    if (!this.collision(1, 0, this.activeTetromino)) {
         this.unDraw();
         this.x++;
         this.draw();
@@ -105,7 +105,7 @@ Piece.prototype.moveRight = function () {
 
 // move Left the piece
 Piece.prototype.moveLeft = function () {
-    if (!this.collision(-1,0,this.activeTetromino)) {
+    if (!this.collision(-1, 0, this.activeTetromino)) {
         this.unDraw();
         this.x--;
         this.draw();
@@ -114,11 +114,11 @@ Piece.prototype.moveLeft = function () {
 
 // rotate the piece
 Piece.prototype.rotate = function () {
-    let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length];
+    let nextPattern = this.tetromino[(this.tetrominoN + 1) % this.tetromino.length];
     let kick = 0;
 
-    if (this.collision(0,0,nextPattern)) {
-        if (this.x > COL/2) {
+    if (this.collision(0, 0, nextPattern)) {
+        if (this.x > COL / 2) {
             // it's the right wall
             kick = -1; // we need to move the piece to the left
         } else {
@@ -127,11 +127,10 @@ Piece.prototype.rotate = function () {
         }
     }
 
-
-    if (!this.collision(kick,0,0,nextPattern)) {
+    if (!this.collision(kick, 0, 0, nextPattern)) {
         this.unDraw();
-        this.x +=kick;
-        this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length; //(0+1)%4 =>1
+        this.x += kick;
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length; //(0+1)%4 =>1
         this.activeTetromino = this.tetromino[this.tetrominoN];
         this.draw();
     }
@@ -139,7 +138,7 @@ Piece.prototype.rotate = function () {
 
 // collision function
 
-Piece.prototype.collision = function(x,y,piece) {
+Piece.prototype.collision = function (x, y, piece) {
     for (r = 0; r < piece.length; r++) {
         for (c = 0; c < piece.length; c++) {
             // if the square is empty, we skip it
@@ -170,18 +169,19 @@ Piece.prototype.collision = function(x,y,piece) {
 
 // CONTROL the piece
 
-document.addEventListener("keydown",CONTROL);
+document.addEventListener("keydown", CONTROL);
+
 function CONTROL(event) {
     if (event.keyCode == 37) {
         p.moveLeft();
         dropStart = Date.now();
-    }else if (event.keyCode == 38) {
+    } else if (event.keyCode == 38) {
         p.rotate();
         dropStart = Date.now();
-    }else if (event.keyCode == 39) {
+    } else if (event.keyCode == 39) {
         p.moveRight()
         dropStart = Date.now();
-    }else if (event.keyCode == 40) {
+    } else if (event.keyCode == 40) {
         p.moveDown()
         dropStart = Date.now();
     }
